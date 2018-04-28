5d774dfa13c4d759740af66380db5309a974ba77
Julia Marden <jmarden16@simons-rock.edu>
2018-04-28 11:30:38 -0400

    made utils.c more readable

--- utils.c
+++ utils.c
@@ -14,32 +14,32 @@ void error(char *msg) {
 /* 
  * Read and process commands
  */
-int process_args(int cmd_argc, char **cmd_argv, QNode **root, Node *interests,
+int process_args(int command_line_argc, char **command_line_argv, QNode **root, Node *interests,
 				 struct client *current_client, struct client *head) {
 	QNode *qtree = *root;
 
-	if (cmd_argc <= 0) {
+	if (command_line_argc <= 0) {
 		return 0;
-	} else if (strcmp(cmd_argv[0], "quit") == 0 && cmd_argc == 1) {
+	} else if (strcmp(command_line_argv[0], "quit") == 0 && command_line_argc == 1) {
 		/* Return an appropriate value to denote that the specified
 		 * user is now disconnected. */
 		return -1;
 		
-	} else if (strcmp(cmd_argv[0], "do_test") == 0 && cmd_argc == 1) {
+	} else if (strcmp(command_line_argv[0], "do_test") == 0 && command_line_argc == 1) {
 		/* The specified user is ready to start answering questions. You
 		 * need to make sure that the user answers each question only
 		 * once.
 		 */
 		
-	} else if (strcmp(cmd_argv[0], "get_all") == 0 && cmd_argc == 1) {
+	} else if (strcmp(command_line_argv[0], "get_all") == 0 && command_line_argc == 1) {
 		/* Return the list of best mismatches related to the specified
 		 * user. If the user has not taked the test yet, return the
 		 * corresponding error value (different than 0 and -1).
 		 */
 		 
-	} else if (strcmp(cmd_argv[0], "post") == 0 && cmd_argc == 3) {
-		/* Send the specified message stored in cmd_argv[1] to the user
-		 * stored in cmd_argv[2].
+	} else if (strcmp(command_line_argv[0], "post") == 0 && command_line_argc == 3) {
+		/* Send the specified message stored in command_line_argv[1] to the user
+		 * stored in command_line_argv[2].
 		 */
 	}
 	else {
@@ -50,22 +50,22 @@ int process_args(int cmd_argc, char **cmd_argv, QNode **root, Node *interests,
 }
 
 /*
- * Tokenize the string stored in cmd.
- * Return the number of tokens, and store the tokens in cmd_argv.
+ * Tokenize the string stored in command_line.
+ * Return the number of tokens, and store the tokens in command_line_argv.
  */
-int tokenize(char *cmd, char **cmd_argv) {
-    int cmd_argc = 0;
-    char *next_token = strtok(cmd, DELIM);    
+int tokenize(char *command_line, char **command_line_argv) {
+    int command_line_argc = 0;
+    char *next_token = strtok(command_line, DELIM);    
     while (next_token != NULL) {
-        if (cmd_argc >= INPUT_ARG_MAX_NUM - 1) {
+        if (command_line_argc >= INPUT_ARG_MAX_NUM - 1) {
             error("Too many arguments!");
-            cmd_argc = 0;
+            command_line_argc = 0;
             break;
         }
-        cmd_argv[cmd_argc] = next_token;
-        cmd_argc++;
+        command_line_argv[command_line_argc] = next_token;
+        command_line_argc++;
         next_token = strtok(NULL, DELIM);
     }
 
-    return cmd_argc;
+    return command_line_argc;
 }
